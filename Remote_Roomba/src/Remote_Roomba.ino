9110add70de9be8947d8f876720bd75a074b5961
Malthe <mac1@Mac1s-MacBook-Pro.local>
2020-10-05 16:17:22 +0200

    Added subscribe event, so photon reacts on IFTTT

--- Remote_Roomba/src/Remote_Roomba.ino
+++ Remote_Roomba/src/Remote_Roomba.ino
@@ -1,18 +1,40 @@
-/*
- * Project Remote_Roomba
- * Description:
- * Author:
- * Date:
- */
+////////////////////////////////////////////////////////////
+//
+// Source file   : Remote_Roomba.ino
+// Author        : Malthe B. Nielsen
+// Date          : 05/10/2020
+// Version       : 0.1
+// 
+// Description   :
+// This is the main file for the RR project, which is a project
+// To make wifi connect a non wifi roomba
+////////////////////////////////////////////////////////////
+
+int LED = D7;
+int i = 0;
+
+// Handling all the events incoming to particle
+void myHandler(const char *event, const char *data)
+{
+  i++;
+  Log.info("%d: event=%s data=%s", i, event, (data ? data : "NULL"));
+  digitalWrite(LED, HIGH);
+  delay(1s);
+}
+
 
 // setup() runs once, when the device is first turned on.
 void setup() {
-  // Put initialization like pinMode and begin functions here.
+  Particle.subscribe("Remote_start", myHandler, ALL_DEVICES);
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
+  digitalWrite(LED, LOW);
+  delay(1s);
 
 }
