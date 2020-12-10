2e2936a44b6517ec62cbc5818d6673f232c8b557
Malthe Nielsen <54708397+Gubimand@users.noreply.github.com>
2020-12-10 12:35:08 +0100

    Update Remote_Roomba.ino with comments

--- Remote_Roomba_Photon/src/Remote_Roomba.ino
+++ Remote_Roomba_Photon/src/Remote_Roomba.ino
@@ -3,7 +3,7 @@
 // Source file   : Remote_Roomba.ino
 // Author        : Malthe B. Nielsen
 // Date          : 05/10/2020
-// Version       : 6.3
+// Version       : 6.4
 // 
 // Description   :
 // This is the main file for the RR project, which is a project
@@ -29,6 +29,7 @@ size_t readBufOffset = 0;
 int receivedValue;
 int waiting;
 
+//Setup function
 void setup()
 {
   // Serial for USB Debugging
@@ -41,51 +42,68 @@ void setup()
 
   //Subscribe to event
   Particle.subscribe("Remote_start", received_publish, ALL_DEVICES);
-
+  
+  // Waiting is true
   waiting = 1;
 }
 
+//Loop function
 void loop()
 {
+  //Print to USB Serial
   Serial.printlnf("Waiting for message from Arduino\n");
   // Wait for respons from Arduino
   while (waiting == 1)
   {
     uart_receive();
   }
+  //Print to USB Serial
   Serial.printlnf("Message recieved from Arduino\n");
   //Publish recieved message from Arduino
   publish_message();
+  // Waiting is true
   waiting = 1;
 }
 
 // UART Receiver.
 void uart_receive()
 {
+  //wait 1 sec
   delay(1000);
-  // Read data from serial
+  // Read data from serial when it is ready
   while (Serial1.available())
   {
+    // Check if readBufOffset have exceed buffer size
     if (readBufOffset < READ_BUF_SIZE)
     {
+      //If it haven't exceeded the buffer size
+      //Read the serial port
       char c = Serial1.read();
+      //Check if newline was recieved
       if (c != '\n')
       {
-        // Add character to buffer
+        // Add character to buffer if it wasn't a newline
         readBuf[readBufOffset++] = c;
       }
+      // If the char recieved was a newline 
       else
       {
         // End of line character found, process line
         readBuf[readBufOffset] = 0;
+        // Call process_received function
         process_received();
+        // Reset buffer counter
         readBufOffset = 0;
+        // Set waiting to false / 0
         waiting = 0;
       }
     }
+    // If readBufOffset have exceed buffer size
     else
     {
+      //Print to USB Serial
       Serial.println("readBuf overflow, emptying buffer\n");
+      // Reset buffer counter
       readBufOffset = 0;
     }
   }
@@ -94,6 +112,7 @@ void uart_receive()
 // UART send
 void uart_send(int data)
 {
+  //Print to USB Serial
   Serial.printlnf("Send Data; %d\n", data);
   // Send data over UART (Serial1) to Arduino.
   Serial1.printlnf("%d", data);
@@ -102,6 +121,7 @@ void uart_send(int data)
 // handler for received publish message
 void received_publish(const char *event, const char *data)
 {
+  //Print to USB Serial
   Serial.printlnf("Catched published message from IFTTT\n");
   //Start Roomba
   uart_send(1);
@@ -116,24 +136,31 @@ void received_publish(const char *event, const char *data)
 */
 void publish_message()
 {
+  // Saves the receivedValue to a local variable
   int x = receivedValue;
+  //Print to USB Serial
   Serial.printlnf("Recieved %d from Arduino", x);
-
+  
+  // Switch statement to determine what message to publish
   switch (x)
   {
   case 1:
+    // Publish message "Roomba deployed." to the topic Roomba
     Particle.publish("Roomba", "Roomba deployed.", PUBLIC);
     break;
 
   case 2:
+    // Publish message "Roomba present but not deployed." to the topic Roomba
     Particle.publish("Roomba", "Roomba present but not deployed.", PUBLIC);
     break;
 
   case 3:
+    // Publish message "Roomba was not present." to the topic Roomba
     Particle.publish("Roomba", "Roomba was not present.", PUBLIC);
     break;
 
   case 4:
+    // Publish message "ERROR: Something went wrong!" to the topic Roomba
     Particle.publish("Roomba", "ERROR: Something went wrong!", PUBLIC);
     break;
 
@@ -145,5 +172,6 @@ void publish_message()
 //Process the received data from UART
 void process_received()
 {
+  // Convert readBuf from char array to integer value
   receivedValue = atoi(readBuf);
 }
